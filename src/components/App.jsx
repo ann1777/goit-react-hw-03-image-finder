5c8b43799bf2803a5924ee2b04f855b68f70a974
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-05-27 20:10:20 +0300

    Fixed modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-
 import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
 import Searchbar from './Searchbar/Searchbar';
-import {ImageGallery} from './ImageGallery/ImageGallery';
+import { ImageGallery } from './ImageGallery/ImageGallery';
 import Modal from './Modal/Modal';
-import { LoadButton } from './LoadButton/LoadButton'; 
+import { LoadButton } from './LoadButton/LoadButton';
 // import Loader from './Loader/Loader';
 import { ToastContainer, toast } from 'react-toastify';
 import handleFetch from '../services/pixabayapi';
@@ -12,18 +12,49 @@ export class App extends Component {
     images: [],
     inputValue: '',
     modalImg: '',
+    status: 'idle',
     showModal: false,
     page: 1,
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.inputValue !== this.state.inputValue ||
-      prevState.page !== this.state.page
-    ) {
-      this.fetchImages();
+  resetPage = () => {
+    this.setState({page: 1});
+  };
+  async componentDidUpdate(_, prevState) {
+    if (this.state.status === 'pending') {
+        const data = await handleFetch(this.state.inputValue).then(
+          this.setState({ status: 'loading' })
+        );
+
+        if (data.hits.length === 12) {
+          return this.setState(
+            prevState.inputValue !== this.state.inputValue
+              ? {
+                  images: data.hits,
+                  status: 'loaded',
+                }
+              : {
+                  images: [...prevState.images, ...data.hits],
+                  status: 'loaded',
+                }
+          );
+        }
+        if (data.hits.length === 0) {
+          return this.setState({ photos: [], status: 'rejected' });
+        }
+        return this.setState(
+          prevState.inputValue !== this.state.inputValue
+            ? {
+                images: data.hits,
+                status: 'idle',
+              }
+            : {
+                images: [...prevState.images, ...data.hits],
+                status: 'idle',
+              }
+        );
+      }
     }
-  }
 
   fetchImages = async () => {
     try {
@@ -32,14 +63,12 @@ export class App extends Component {
       this.setState({
         isLoading: true,
       });
-    
 
       const images = await handleFetch(inputValue, page);
       console.log(images);
 
       this.setState(prevState => ({
-        images:
-            [...prevState.images, ...images.hits],
+        images: [...prevState.images, ...images.hits],
         isLoading: false,
         totalPage: Math.ceil(images.total / 12),
       }));
@@ -56,37 +85,52 @@ export class App extends Component {
   };
 
   getInputValue = handleValue => {
-    this.setState({ inputValue: handleValue, page: 1, images: [] })
-  }
+    this.resetPage();
+    this.setState({ status: 'pending', inputValue: handleValue });
+  };
 
   toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }))
-  }
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
 
   getLargeImg = url => {
     this.toggleModal();
     this.setState({ modalImg: url });
-  }
+  };
 
   onLoadMore = () => {
+    this.setState({ status: 'pending' });
     this.setState(prevState => ({
       page: prevState.page + 1,
     }));
   };
 
-  render () {
-
+  render() {
     const { modalImg, showModal, page, totalPage, images } = this.state;
     const loadMoreImgs = page < totalPage;
-    console.log(showModal, modalImg)
+    console.log(showModal, modalImg);
     return (
       <>
-        <Searchbar getInputValue={this.getInputValue}/>
-        <ImageGallery images={images} toggleModal={this.getLargeImg}/>
-        {showModal && <Modal url={modalImg} onClose={this.toggleModal}/>}
+        <Searchbar getInputValue={this.getInputValue} />
+        <ImageGallery images={images} toggleModal={this.getLargeImg} />
+        {showModal && createPortal(<Modal url={modalImg} onClose={this.toggleModal} />,
+            document.body
+          )}
+        {this.state.status === 'loaded' && (
+          <LoadButton onLoadMore={this.onLoadMore} />
+        )}
+        {this.state.status === 'rejected' && (
+          <div>
+            Your generic alert to promt you that there are no images found, but
+            I was too lazy to style it. Hell, at least it removed that "Load
+            More" button from showing
+          </div>
+        )}  
         {loadMoreImgs && <LoadButton onLoadMore={this.onLoadMore} />}
         <ToastContainer autoClose={1000} />
       </>
-    )
+    );
   }
 }
+
+export default App;
